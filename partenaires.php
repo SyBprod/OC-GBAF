e42598a8d75650389fed3fd6064c15ac9eb368c6
SyBprod <syb.prod@gmail.com>
2021-05-19 11:15:59 +0200

    Insertion post dans table post et affichage des post dans page partenaire. Fonctionne mais avec des problèmes...

--- partenaires.php
+++ partenaires.php
@@ -1,23 +1,4 @@
-<?php //Connexion à la bdd
-    try{
-        $bdd = new PDO('mysql:host=localhost;dbname=oc_gbaf;charset=utf8', 'root', '', 
-        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        }
-        catch(Exception $e)
-        {
-            die('Erreur : ' .$e->getMessage());
-        }
-        if(isset($_GET['id_acteur']))
-        {
-            $id_acteur = $_GET['id_acteur'];
-            echo $id_acteur;
-        }
-        $req = $bdd->prepare('SELECT acteur, description, logo FROM acteur WHERE id_acteur = :id_acteur');
-        $req->execute(array(
-            'id_acteur' => $id_acteur));
-        $resultat = $req->fetch();
-        //Récupération des infos de la table acteur
-?>
+
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -32,7 +13,28 @@
     </head>
     <body>
         <?php include('header.php'); ?>
-        <main>   
+        <main> 
+        <?php 
+            //Connexion à la bdd
+            try{
+                $bdd = new PDO('mysql:host=localhost;dbname=oc_gbaf;charset=utf8', 'root', '', 
+                                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                }
+            catch(Exception $e){
+                die('Erreur : ' .$e->getMessage());
+            }
+            if(isset($_GET['id_acteur']))//On teste pour savoir si la variable id_acteur existe
+            {
+                $_SESSION['id_acteur'] = $_GET['id_acteur'];
+                var_dump($_SESSION);
+            }
+            //Récupération des infos de la table acteur avec une requète préparée sur la base de l'id_acteur
+            $req = $bdd->prepare('SELECT acteur, description, logo FROM acteur WHERE id_acteur = :id_acteur');
+            $req->execute(array(
+                'id_acteur' => $_SESSION['id_acteur']
+            ));
+            $resultat = $req->fetch();
+        ?>  
             <section class="wrapper">
                 <div class="mt-20 mb-20"> 
                 <!--Récupère le LOGO dans la table ACTEUR-->
@@ -42,9 +44,9 @@
                     <h1 class="pt-20 pb-20"><?php echo $resultat['acteur']; ?></h1>
                     <!--Récupère le texte DESCRIPTION dans la table ACTEUR-->
                     <p class="pt-20"><?php echo $resultat['description']?></p>
-<?php
-    $req->closeCursor();
-?>
+        <?php //On ferme la requète pour pouvoir en ouvrir une autre après  
+            $req->closeCursor();
+        ?>
                 <div class="liste-part">
                     <div class="part-content-social">
                         <div class="social">
@@ -54,25 +56,46 @@
                             <div class="bt-comment">
                                 <a class="button" href="#commenter" alt="commenter"><i class="far fa-comment-alt"></i>Commenter</a>
                             </div>
+                            <!--Script de prise en compte des votes-->
+                            <?php
+                            //if(isset($_GET['positif]') AND )
+                               //                             $_GET['positif'] $_GET['negatif']
+                            ?>
                             <div class="likes">
-                           
-                                <span>11</span><a href="#"><i class="fas fa-thumbs-up"></i></a>
-                                <span>10</span><a href="#"><i class="fas fa-thumbs-down"></i></a>
+                                <span>11</span><a href="partenaires.php?vote=positif"><i class="fas fa-thumbs-up"></i></a>
+                                <span>10</span><a href="partenaires.php?vote=negatif"><i class="fas fa-thumbs-down"></i></a>
                             </div>
                         </div>
                     </div>
+        <?php //Récupération des infos dans la table post pour afficher les posts relatifs à l'id_acteur
+            //$id_acteur = $_GET['id_acteur'];
+            if(isset($_SESSION['id_acteur']))
+            {
+            $req = $bdd->prepare('SELECT id_user, date_add, post FROM post WHERE id_acteur = :id_acteur');//pour le prenom voir si je peux faire appel aux SESSIONS
+            $req->execute(array(
+                'id_acteur' => $_SESSION['id_acteur']
+            ));
+            
+            while($reponse = $req->fetch())
+            {
+        ?>
                     <div class="part-content">
                         <div class="part-text">
-                            <p>De : </p>
-                            <p>Le : </p> 
-                            <p>Commentaire : </p>
+                            <p><strong>De : </strong><?php echo $reponse['id_user']; ?></p>
+                            <p><strong>Le : </strong><?php echo $reponse['date_add']; ?></p> 
+                            <p><strong>Commentaire : </strong><br /><?php echo nl2br(htmlspecialchars($reponse['post'])); ?></p>
                         </div>
                     </div>
-                    <form id="commenter" action="#" method="POST">
+        <?php   }
+            }
+       // Fin de la boucle des commentaires
+            $req->closeCursor();
+        ?>
+                    <form id="commenter" action="post_post.php" method="POST"><!--Formulaire pour poster des commentaires INSERT INTO post-->
                         <label for="prenom">Votre prénom :</label><br />
-                        <input type="text" id="prenom" name="prenom">
+                        <input type="text" id="prenom" name="prenom" value="<?php if(isset($_SESSION['prenom'])) echo $_SESSION['prenom'];?>">
                         <label for="post">Votre message :</label><br />
-                        <textarea  id="post" name="post" style="width:100%; height:100px;"> 
+                        <textarea  id="post" name="post" style="width:100%; height:100px;" value="<?php if(isset($_POST['post'])) echo htmlspecialchars($_POST['post']); ?>"> 
                         </textarea>
                         <input type="submit" value="Envoyer">
                     </form>
---
+++ post_post.php
@@ -0,0 +1,31 @@
+
+<?php //Connexion bdd
+    try{
+        $bdd = new PDO('mysql:host=localhost;dbname=oc_gbaf;charset=utf8', 'root', '', 
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }
+        catch(Exception $e)
+        {
+            die('Erreur : ' .$e->getMessage());
+        }
+
+   
+     //if(isset($_SESSION['id_user']) AND isset($_SESSION['id_acteur']) AND !empty($_POST['post'])){}
+        $id_user = $_SESSION['id_user'];
+        $id_acteur = $_SESSION['id_acteur'];
+        $post = $_POST['post'];
+        var_dump($_SESSION);
+
+    
+    $req = $bdd->prepare('INSERT INTO post(id_user, id_acteur, date_add, post) VALUES (:id_user, :id_acteur, NOW(), :post)');
+    $req->execute(array(
+        ':id_user' => $id_user,
+        ':id_acteur' => $id_acteur,
+        ':post' => $post
+    ));
+
+    $req->closeCursor();
+header('Location: partenaires.php');
+?>
+
+                  
